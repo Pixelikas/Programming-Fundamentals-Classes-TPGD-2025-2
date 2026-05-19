c4838b427a21234fe88431066e399c9bcca21566
Lucas Naspolini <67108278+Pixelikas@users.noreply.github.com>
2026-05-18 23:11:59 -0300

    Add files via upload

---
+++ Serialização JSON/Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerClass
+{
+    public string charName;
+    public int charAge;
+    public float charHeight;
+    public bool charDash;
+
+}
